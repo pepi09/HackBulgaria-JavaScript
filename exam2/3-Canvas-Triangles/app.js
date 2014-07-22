1f8d482e56e4bed9b2e6ec5f09b3ec6ae16059a6
pepi09 <pepa.p.simeonova@abv.bg>
2014-07-22 13:57:51 +0300

    drawing triangles

--- exam2/3-Canvas-Triangles/app.js
+++ exam2/3-Canvas-Triangles/app.js
@@ -1,29 +1,63 @@
 "use strict";
 
-var c = document.getElementById("canvas");
-var ctx = c.getContext('2d');
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext('2d');
+var points = []
 
-function Tile(x, y, ctx){
+function Tile(x, y, ctx) {
         this.x = x;
         this.y = y;
         this.ctx = ctx;
 
-        this.print = function(){
+        this.print = function() {
             ctx.fillStyle = "red";
-            //ctx.arc(this.x, this.y, 10, 10, Math.PI * 2);
             ctx.fillRect(this.x,this.y, 1, 1);
         }
 }
 
-var r = new Tile(10,10, ctx);
-r.print();
+function print_line(point1, point2, ctx) {
+      ctx.fillStyle = "red";
+      ctx.beginPath();
+      ctx.moveTo(point1.x, point1.y);
+      ctx.lineTo(point2.x, point2.y);
+      ctx.stroke();
+}
 
 $("#canvas").on("click",function( event ){
   var p = new Tile(event.pageX, event.pageY, ctx);
+  points.push(p);
+  if (points.length === 3){
+    make_triangle(points);
+    points.splice(0,3);
+  }
   p.print();
-  console.log("ha ha");
 })
 
+var make_triangle = function(pointsArr){
+  if (isItTriangle(pointsArr[0],pointsArr[1],pointsArr[2])) {
+    print_line(pointsArr[0], pointsArr[1], ctx);
+    print_line(pointsArr[1], pointsArr[2], ctx);
+    print_line(pointsArr[2], pointsArr[0], ctx);
+  }
+
+}
 
 var isItTriangle = function(point1, point2, point3) {
+  var a = side_length(point1,point2),
+      b = side_length(point2,point3),
+      c = side_length(point3,point1);
+
+  if (((a + b) > c) && ((b + c) > a) && ((a + c) > b)){
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
+
+var side_length = function(point1, point2) {
+  var a = point1.x - point2.x,
+      b = point1.y - point2.y;
+  return Math.sqrt(a*a + b*b);
 }
